947515556e9362fa11fc418529a3ab77fa15e30e
zaki-yama <shingoyamazaki00@gmail.com>
2016-01-08 00:25:00 +0900

    サイト名変更

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://zaki-yama.github.io/salesforce-developers-info'
+SITEURL = 'http://zaki-yama.github.io/salesforce-developers.info'
 RELATIVE_URLS = False
 
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
